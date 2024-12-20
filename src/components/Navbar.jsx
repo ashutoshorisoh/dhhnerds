b441de303b4e504d4a907b576f5df2282462402b
Ashutosh <ashutoshorisoh@gmail.com>
2024-12-20 13:46:08 +0530

    navbarlogoadded

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -28,38 +28,44 @@ export default function Navbar() {
 
   return (
     <div className="w-full top-0 fixed flex justify-center items-center pt-2 h-20 overflow-y-hidden overflow-x-hidden bg-black z-50">
-      <div className="flex-row gap- fixed w-full overflow-hidden h-24 pt-10 pb-10 flex items-center text-sm justify-end pl-2 pr-2 text-white">
-        <h1
-          onClick={() => handleNavigation("/")}
-          className="hover:p-2 flex justify-center hover:text-md hover:rounded-md hover:shadow-md hover:shadow-stone-600 hover:border hover:border-white hover:overflow-hidden cursor-pointer w-28"
-        >
-          Home
-        </h1>
-
-        <h1
-  onClick={() => handleNavigation("/signup")}
-  className={`hover:p-2 flex justify-center hover:text-md hover:rounded-md hover:shadow-md hover:shadow-stone-600 hover:border hover:border-white hover:overflow-hidden cursor-pointer w-28 ${userEmail ? 'hidden' : ''}`}
->
-  Sign Up
-</h1>
-
-
-        {userEmail ? (
-          <h1
-            onClick={logout}
-            className="hover:p-2 flex justify-center hover:text-md hover:rounded-md hover:shadow-md hover:shadow-stone-600 hover:border hover:border-white hover:overflow-hidden cursor-pointer w-28"
-          >
-            Logout
-          </h1>
-        ) : (
-          <h1
-            onClick={() => handleNavigation("/login")}
-            className="hover:p-2 flex justify-center hover:text-md hover:rounded-md hover:shadow-md hover:shadow-stone-600 hover:border hover:border-white hover:overflow-hidden cursor-pointer w-28"
-          >
-            Login
-          </h1>
-        )}
-      </div>
+  <div className="flex-row gap- fixed w-full overflow-hidden h-24 pt-10 pb-10 flex items-center text-sm justify-between pl-2 pr-2 text-white">
+    <div className=" h-40 w-40 overflow-hidden">
+      <img src="/logo.png" alt="" className="h-full w-full object-contain" />
     </div>
+    <div className="flex">
+    <h1
+      onClick={() => handleNavigation("/")}
+      className="hover:p-2 flex justify-center hover:text-md hover:rounded-md hover:shadow-md hover:shadow-stone-600 hover:border hover:border-white hover:overflow-hidden cursor-pointer w-28"
+    >
+      Home
+    </h1>
+
+    <h1
+      onClick={() => handleNavigation("/signup")}
+      className={`hover:p-2 flex justify-center hover:text-md hover:rounded-md hover:shadow-md hover:shadow-stone-600 hover:border hover:border-white hover:overflow-hidden cursor-pointer w-28 ${userEmail ? 'hidden' : ''}`}
+    >
+      Sign Up
+    </h1>
+
+    {userEmail ? (
+      <h1
+        onClick={logout}
+        className="hover:p-2 flex justify-center hover:text-md hover:rounded-md hover:shadow-md hover:shadow-stone-600 hover:border hover:border-white hover:overflow-hidden cursor-pointer w-28"
+      >
+        Logout
+      </h1>
+    ) : (
+      <h1
+        onClick={() => handleNavigation("/login")}
+        className="hover:p-2 flex justify-center hover:text-md hover:rounded-md hover:shadow-md hover:shadow-stone-600 hover:border hover:border-white hover:overflow-hidden cursor-pointer w-28"
+      >
+        Login
+      </h1>
+    )}
+    </div>
+    
+  </div>
+</div>
+
   );
 }
